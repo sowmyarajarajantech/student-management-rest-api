85c4046444f0e3b93203904f7b1b79350b51b8a8
itsthatextrovert <sowmyarajarajan.tech@gmail.com>
2026-01-07 00:59:52 +0530

    Migrate database from H2 to MySQL

--- src/test/java/com/example/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/com/example/studentmanagement/StudentManagementApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.studentmanagement;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class StudentManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
